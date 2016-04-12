9e4ba1c2eda6b4276f0097b814e63357dabf6699
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-04-12 15:13:10 +0200

    small incremental update to ReturnKthToLast

--- SolutionToCtCI/Chapter2/ReturnKthToLast.java
+++ SolutionToCtCI/Chapter2/ReturnKthToLast.java
@@ -4,30 +4,29 @@ import CtCILibrary.AssortedMethods;
 import CtCILibrary.LinkedListNode;
 
 public class ReturnKthToLast {
-	public static void printKthToLast(LinkedListNode head, int k){
-		int len = 0;
-		LinkedListNode n = head;
-		while (n!=null) {
-			len ++;
-			n = n.next;
+	public static LinkedListNode returnKthToLast(LinkedListNode head, int k){
+		LinkedListNode end = head;
+		LinkedListNode kth = head;
+		for (int i = 0; i < k; i++) {
+			if (end == null) return null;
+			end = end.next;
 		}
-		n = head;
-		for (int i = 0; i< len; i++) {
-			if (i == (len-k)) {
-				System.out.println(k + "th to last node is "+n.data);
-			}
-			n = n.next;
+		while (end != null) {
+			end = end.next;
+			kth = kth.next;
 		}
+		return kth;
+
 	}
 	public static void main(String[] args) {
 		int[] array = {0, 1, 2, 3, 4, 5, 6};
 		LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
 		for (int i = 0; i <= array.length + 1; i++) {
-			printKthToLast(head, i);
+			LinkedListNode node = returnKthToLast(head, i);
+			String nodeValue = node == null ? "null" : "" + node.data;
+			System.out.println(i + ": " + nodeValue);
 		}
 	}
-	
-
 }
 /*
  * Space Complexity O(1) 
